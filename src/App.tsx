e5828fd2c30127a5d146df81158c9fdeeaa86d98
Sarthak Bhan <sarthakcr007@gmail.com>
2023-12-04 00:52:16 +0530

    added signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { Routes, Route } from 'react-router-dom';
+
+import SigninForms from './_auth/forms/SigninForms';
+import SignupForm from './_auth/forms/SignupForm';
+import { Home } from './_root/pages';
+import './globals.css';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className="flex h-screen">
+        <Routes>
+            {/* public routes */}
+            <Route element={<AuthLayout />}>
+                <Route path="/sign-up" element={<SignupForm />} />
+                <Route path="/sign-in" element={<SigninForms />} /> 
+            </Route>
+
+            {/* private routes */}
+            <Route element={<RootLayout />}>
+                <Route index element={<Home />} />
+            </Route>
+        </Routes>
+    </main>
   )
 }
 
